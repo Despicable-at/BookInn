fce3fd1d1afc179a44749615a3baaa23a49246b1
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 21:27:41 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import './Navbar.css';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">Bookinn</div>
-      <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact Us</a></li>
-      </ul>
-    </nav>
+    <header>
+      <div className="logo"><span>B</span>OOKINN</div>
+      <nav>
+        <Link to="/" className="active">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
+      </nav>
+    </header>
   );
-};
+}
 
 export default Navbar;
