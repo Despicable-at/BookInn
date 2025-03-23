9a175f3b6d78e9aa1ccf03486a24cb72a726036a
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 23:09:40 +0000

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,17 +6,21 @@ function Hero() {
       className="hero"
       style={{ backgroundImage: 'url(https://bookinn-sc77.onrender.com/assets/12375.jpg)' }} // Set the background image
     >
-      <div className="overlay"></div> {/* Add overlay */}
-      <h1>Welcome to <span className="highlight">Bookinn</span> – Your Ultimate Accommodation Hub</h1>
-      <div className="app-buttons">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
-          alt="Google Play"
-        />
-        <img
-          src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
-          alt="App Store"
-        />
+      <div className="overlay"></div>
+      <div className="hero-content">
+        <h1>Welcome to <span className="highlight">Bookinn</span> – Your Ultimate Accommodation Hub</h1>
+        <div className="app-buttons">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+            alt="Google Play"
+            className="app-btn"
+          />
+          <img
+            src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+            alt="App Store"
+            className="app-btn"
+          />
+        </div>
       </div>
     </section>
   );
