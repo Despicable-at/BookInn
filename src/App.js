b54b4e754a9fd33276625d03995b9690e8941cb3
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 20:20:42 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/Contact'; // Update the path if necessary
 
 function App() {
   return (
