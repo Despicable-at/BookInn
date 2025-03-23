226642f43ec909012a105259aaea14ae614b3022
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 20:59:41 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Changed this import
+import ReactDOM from 'react-dom/client';  // Make sure to import from 'react-dom/client'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// This is for React 18+ rendering method
+// Using React 18 rendering method
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
@@ -11,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
