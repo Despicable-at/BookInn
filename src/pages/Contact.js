c23888d65c203b9501087604cd0726e9ad20e574
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 20:17:37 +0000

    Create Contact.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>We would love to hear from you! Reach out with any questions or feedback.</p>
+    </div>
+  );
+};
+
+export default Contact;
