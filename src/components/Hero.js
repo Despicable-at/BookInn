ac2abf4b473c7804c6a49b07ef79420d854c3137
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 21:30:09 +0000

    Create Hero.js

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Welcome to <span className="highlight">Bookinn</span> – Your Ultimate Accommodation Hub</h1>
+      <div className="app-buttons">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Google Play" />
+        <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="App Store" />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
