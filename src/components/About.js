d5276378cc954d1b9f492a7b23580a0bfcabe917
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-23 21:30:28 +0000

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Us</h2>
+      <p>Bookinn helps you find the best accommodations effortlessly. Discover, compare, and book easily.</p>
+    </section>
+  );
+}
+
+export default About;
