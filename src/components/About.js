0b8933a28b64cd371aa19b381d6fd651ea7d2b4d
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-24 01:11:54 +0000

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function About() {
   return (
-    <section id="about" className="about">
+    <section
+      id="about"
+      className="about"
+      style={{
+        backgroundImage: 'url(https://bookinn-sc77.onrender.com/assets/background.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <h2>About Us</h2>
       <p>Bookinn helps you find the best accommodations effortlessly. Discover, compare, and book easily.</p>
     </section>
