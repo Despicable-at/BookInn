4e40e29cd0082ee7989431ff7e5b787c62a43347
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-24 00:54:40 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <header>
-      <div className="logo"><span>B</span>OOKINN</div>
+      <div className="logo">
+        <img src="https://bookinn-sc77.onrender.com/assets/Frame.png" alt="Bookinn Logo" /> {/* Replace with your image path */}
+      </div>
       <nav>
         <Link to="/" className="active">Home</Link>
         <Link to="/about">About</Link>
