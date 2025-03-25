226c27d39884864a1cd6159a26236e10ac0f29db
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-25 02:39:05 +0000

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,24 @@ function Contact() {
   return (
     <section id="contact" className="contact">
       <h2>Send Us a Message Today</h2>
-      <form>
-        <input type="text" placeholder="Name" />
-        <div className="phone-input">
-          <span>🇬🇭 +233</span>
-          <input type="tel" placeholder="XX-XXX-XXXX" />
-        </div>
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Message here"></textarea>
-        <button type="submit">Send</button>
-      </form>
+      <div class="contact-form">
+          <h2>Send Us a Message Today</h2>
+          
+          <div class="form-row">
+            <input type="text" placeholder="Name" class="input name">
+            <div class="phone-input">
+              <span class="flag">🇬🇭 +233</span>
+              <input type="text" placeholder="XX-XXX-XXXX" class="input phone">
+            </div>
+          </div>
+          
+          <input type="email" placeholder="Email" class="input full-width">
+          
+          <textarea placeholder="Message here" class="input full-width"></textarea>
+          
+          <button class="send-btn">Send</button>
+      </div>
+
       <div className="contact-img">
         <img src="https://static.vecteezy.com/system/resources/previews/007/581/047/original/search-house-for-rent-or-sale-illustration-concept-free-vector.jpg" alt="Contact Illustration" />
       </div>
