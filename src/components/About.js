9777faf79baef9b02f9e2191c9ca95f3be1b5692
Despicable-at <agyemangtakyi2021@gmail.com>
2025-03-29 16:25:18 +0000

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -44,7 +44,7 @@ function About() {
             className="app-btn1"
           />
           <img
-            src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+            src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Apple-badge%402x.png"
             alt="App Store"
             className="app-btn1"
           />
